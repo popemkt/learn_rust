cb2e54f60f2d9b682c61fdc68cae77e5853dfd63
Nguyen Gia Hoang <popemkt1@gmail.com>
2023-10-08 13:04:11 +0700

    Guessing game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,33 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the number");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Please input your guess");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: i32 = match guess.trim().parse() {
+            Result::Ok(num) => num,
+            Result::Err(_) => continue,
+        };
+        println!("you guessed: {guess}");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
+
